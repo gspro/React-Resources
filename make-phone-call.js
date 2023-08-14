bf92c93619c0383f7adfc8f03c28f2f56128a3a3
gspro <gspross@gmail.com>
2023-08-14 07:25:57 -0400

    Create make-phone-call.js

---
+++ make-phone-call.js
@@ -0,0 +1,31 @@
+import { Linking, Alert, Platform } from 'react-native';
+
+export const callNumber = phone => {
+  console.log('callNumber ----> ', phone);
+  let phoneNumber = phone;
+  if (Platform.OS !== 'android') {
+    phoneNumber = `telprompt:${phone}`;
+  }
+  else  {
+    phoneNumber = `tel:${phone}`;
+  }
+  Linking.canOpenURL(phoneNumber)
+  .then(supported => {
+    if (!supported) {
+      Alert.alert('Phone number is not available');
+    } else {
+      return Linking.openURL(phoneNumber);
+    }
+  })
+  .catch(err => console.log(err));
+};
+
+/*
+Don't forget added to Info.plist ->
+
+<key>LSApplicationQueriesSchemes</key> 
+<array> 
+  <string>tel</string> 
+  <string>telprompt</string> 
+</array>
+*/
